1fc477650c21a602cd5aee59cccf21aeeaab2c61
belkamydog <belkamydog22@gmail.com>
2023-02-05 19:51:58 +0300

    add glider gun & user menu

--- main.c
+++ main.c
@@ -19,15 +19,6 @@ int **create(void){
     return arr;
 }
 
-void output(int **arr){
-    for (int i = 0; i < ROW; i++){
-        for (int j = 0; j < COL; j++){
-            printf("%2d", arr[i][j]);
-        }
-        printf("\n");
-    }
-}
-
 void copy(int **arr, int **new_arr){
     for (int i = 0; i < ROW; i++){
         for (int j = 0; j < COL; j++){
@@ -84,39 +75,52 @@ void screen(int **arr){
     }
 }
 
-void take_start_position(int **start, int **arr){
-	for (int i = 0; i < ROW; i++){
-		for (int j = 0; j < COL; j++){
-			
-		}
-	}
-}
-
 void alive_structures(int **arr){
-	printf("Hellow dear user!\n You have access to 3 organismes\n input:\n 1 for GLIDER\n 2 for BUS\n 3 for KARAVAI\n");
+	printf("Hellow dear user!\n You have access to 3 organismes\n input:\n 1 for GLIDER\n 2 for BUS\n 3 for KARAVAI\n 4 for GLIDER GUN\n Else for all");
 	int choice;
 	scanf("%d", &choice);
-	switch (choice){
-		case 1:
+	if (choice == 1){
 			    arr[3][10] = 1; arr[3][11] = 1; arr[3][12] = 1; arr[2][12] = 1; arr[1][11] = 1; // GLIDER
-		case 2:
-			    arr[4][10] = 1; arr[5][11] = 1; arr[5][12] = 1; arr[5][13] = 1; arr[5][14] = 1; arr[4][14] = 1; arr[3][14] = 1; arr[2][13] = 1;  // bus
-		case 3:
+			}
+	else if (choice == 2){
+			    arr[4][10] = 1; arr[5][11] = 1; arr[5][12] = 1; arr[5][13] = 1; arr[5][14] = 1;
+			     arr[4][14] = 1; arr[3][14] = 1; arr[2][13] = 1;  // bus
+			}
+	else if (choice == 3){
 			   arr[9][40] = 1;arr[10][39] = 1;arr[10][41] = 1;arr[11][38] = 1;arr[11][41] = 1;arr[12][39] = 1;arr[12][40] = 1;
     			   arr[13][38] = 1;arr[13][41] = 1;arr[14][38] = 1;arr[14][40] = 1;arr[15][39] = 1; // karavai
-    		default:
+    			 }
+    	else if (choice == 4){ 
+    			  arr[4][0]= 1; arr[5][0] = 1; arr[4][1] = 1; arr[5][1] = 1;  arr[4][10] = 1;  arr[5][10] = 1;  arr[6][10] = 1; 
+    			  arr[3][11] = 1; arr[7][11] = 1; arr[2][12] = 1;  arr[8][12] = 1;  arr[2][13] = 1;  arr[8][13] = 1; arr[5][14] = 1;
+    			  arr[3][15] = 1; arr[7][15] = 1; arr[4][16] = 1; arr[5][16] = 1; arr[6][16] = 1;  arr[5][17] = 1;  arr[2][20] = 1; 
+    			  arr[3][20] = 1; arr[4][20] = 1; arr[2][21] = 1; arr[3][21] = 1;
+    			  arr[4][21] = 1; arr[1][22] = 1; arr[5][22] = 1; arr[0][24] = 1;  // GLIDER GUN
+    			  arr[1][24] = 1; arr[5][24] = 1; arr[6][24] = 1;
+    			  arr[2][34] = 1;  arr[3][34] = 1; arr[2][35] = 1; arr[3][35] = 1;
+    			 }
+    	else{
     			   arr[9][40] = 1;arr[10][39] = 1;arr[10][41] = 1;arr[11][38] = 1;arr[11][41] = 1;arr[12][39] = 1;arr[12][40] = 1;
     			   arr[13][38] = 1;arr[13][41] = 1;arr[14][38] = 1;arr[14][40] = 1;arr[15][39] = 1; // karavai
     			   arr[3][10] = 1; arr[3][11] = 1; arr[3][12] = 1; arr[2][12] = 1; arr[1][11] = 1; // GLIDER
-	}
+    			   arr[4][10] = 1; arr[5][11] = 1; arr[5][12] = 1; arr[5][13] = 1; arr[5][14] = 1; arr[4][14] = 1;
+    			   arr[3][14] = 1; arr[2][13] = 1;  // bus
+    			   arr[4][0]= 1; arr[5][0] = 1; arr[4][1] = 1; arr[5][1] = 1;  arr[4][10] = 1;  arr[5][10] = 1;  arr[6][10] = 1; 
+    			   arr[3][11] = 1; arr[7][11] = 1; arr[2][12] = 1;  arr[8][12] = 1;  arr[2][13] = 1;  arr[8][13] = 1; arr[5][14] = 1;
+    			   arr[3][15] = 1; arr[7][15] = 1; arr[4][16] = 1; arr[5][16] = 1; arr[6][16] = 1;  arr[5][17] = 1;  arr[2][20] = 1; 
+    			   arr[3][20] = 1; arr[4][20] = 1; arr[2][21] = 1; arr[3][21] = 1;
+    			   arr[4][21] = 1; arr[1][22] = 1; arr[5][22] = 1; arr[0][24] = 1;  // GLIDER GUN
+    			   arr[1][24] = 1; arr[5][24] = 1; arr[6][24] = 1;
+    			   arr[2][34] = 1;  arr[3][34] = 1; arr[2][35] = 1; arr[3][35] = 1;
+    			   }
 		
 }
 
 int main(int argc, int **argv)
 {   char input;
     int **arr = create();
-    alive_structures(arr);
     int **new_arr = create();
+    alive_structures(arr);
     stdscr = initscr();
     while (input != 'q'){
     	timeout(SPEED);
